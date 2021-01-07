3370b1b533817e9c2f1aea172d90fd00565db1d9
Michele Zucca <michele.zucca81@gmail.com>
2021-01-07 12:04:48 +0200

    added meta scripts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,9 @@ module.exports = {
     description: `michele-zucca-portfolio`,
     author: `@michelezucca`,
     type: process.env.GATSBY_ACTIVE_ENV || 'staging',
-    siteUrl: `https://practical-albattani-477e3c.netlify.app/`,
-    hostname: `your-app-domain.netlify.app`,
-    siteUrl: `https://practical-albattani-477e3c.netlify.app/`,
-    twitterHandle: "@michelezucca-portfoliohata",
+    siteUrl: `https://michelezuccawebdev.netlify.app/`,
+    hostname: `https://michelezuccawebdev.netlify.app/`,
+    twitterHandle: '@michelezucca-portfoliohata',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -53,18 +52,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-scroll-reveal`,
       options: {
-          threshold: 1, // Percentage of an element's area that needs to be visible to launch animation
-          once: true, // Defines if animation needs to be launched once
-          disable: false, // Flag for disabling animations
-          
-          // Advanced Options
-          selector: '[data-sal]', // Selector of the elements to be animated
-          animateClassName: 'sal-animate', // Class name which triggers animation
-          disabledClassName: 'sal-disabled', // Class name which defines the disabled state
-          rootMargin: '0% 50%', // Corresponds to root's bounding box margin
-          enterEventName: 'sal:in', // Enter event name
-          exitEventName: 'sal:out', // Exit event name
-      }
+        threshold: 1, // Percentage of an element's area that needs to be visible to launch animation
+        once: true, // Defines if animation needs to be launched once
+        disable: false, // Flag for disabling animations
+
+        // Advanced Options
+        selector: '[data-sal]', // Selector of the elements to be animated
+        animateClassName: 'sal-animate', // Class name which triggers animation
+        disabledClassName: 'sal-disabled', // Class name which defines the disabled state
+        rootMargin: '0% 50%', // Corresponds to root's bounding box margin
+        enterEventName: 'sal:in', // Enter event name
+        exitEventName: 'sal:out', // Exit event name
+      },
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-less`,
